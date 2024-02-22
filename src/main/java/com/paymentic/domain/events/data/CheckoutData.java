0a9cffe55506dc29f7912555b55bde85bb85bcad
claudioed <claudioed.oliveira@gmail.com>
2024-02-21 21:52:06 -0300

    publish one event each payment order

--- src/main/java/com/paymentic/domain/events/data/CheckoutData.java
+++ src/main/java/com/paymentic/domain/events/data/CheckoutData.java
@@ -2,6 +2,8 @@ package com.paymentic.domain.events.data;
 
 import com.paymentic.domain.shared.BuyerInfo;
 import com.paymentic.domain.shared.CardInfo;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record CheckoutData(UUID id, BuyerInfo buyerInfo, CardInfo cardInfo,String idempotencyKey) { }
+public record CheckoutData(UUID id, BuyerInfo buyerInfo, CardInfo cardInfo, String idempotencyKey,
+                           LocalDateTime at) { }
